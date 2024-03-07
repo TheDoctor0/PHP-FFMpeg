561588c7b9374eb9457be8470ccd326e4010f301
TheDoctor0 <dawid.janik95@gmail.com>
2024-03-07 09:16:26 +0100

    Fix invalid number of frames being created by ExtractMultipleFramesFilter.

--- src/FFMpeg/Filters/Video/ExtractMultipleFramesFilter.php
+++ src/FFMpeg/Filters/Video/ExtractMultipleFramesFilter.php
@@ -12,7 +12,6 @@
 namespace FFMpeg\Filters\Video;
 
 use FFMpeg\Exception\InvalidArgumentException;
-use FFMpeg\Exception\RuntimeException;
 use FFMpeg\Format\VideoInterface;
 use FFMpeg\Media\Video;
 
@@ -101,50 +100,9 @@ class ExtractMultipleFramesFilter implements VideoFilterInterface
      */
     public function apply(Video $video, VideoInterface $format)
     {
-        $commands = [];
-        $duration = 0;
-
-        try {
-            // Get the duration of the video
-            foreach ($video->getStreams()->videos() as $stream) {
-                if ($stream->has('duration')) {
-                    $duration = $stream->get('duration');
-                }
-            }
-
-            // Get the number of frames per second we have to extract.
-            if (false !== preg_match('/(\d+)(?:\s*)([\+\-\*\/])(?:\s*)(\d+)/', $this->frameRate, $matches)) {
-                $operator = $matches[2];
-
-                switch ($operator) {
-                    case '/':
-                        $nbFramesPerSecond = $matches[1] / $matches[3];
-                        break;
-
-                    default:
-                        throw new InvalidArgumentException('The frame rate is not a proper division: '.$this->frameRate);
-                        break;
-                }
-            }
-
-            // Set the number of digits to use in the exported filenames
-            $nbImages = ceil($duration * $nbFramesPerSecond);
-
-            if ($nbImages < 100) {
-                $nbDigitsInFileNames = '02';
-            } elseif ($nbImages < 1000) {
-                $nbDigitsInFileNames = '03';
-            } else {
-                $nbDigitsInFileNames = '06';
-            }
-
-            // Set the parameters
-            $commands[] = '-vf';
-            $commands[] = 'fps='.$this->frameRate;
-            $commands[] = $this->destinationFolder.'frame-%'.$nbDigitsInFileNames.'d.'.$this->frameFileType;
-        } catch (RuntimeException $e) {
-            throw new RuntimeException('An error occured while extracting the frames: '.$e->getMessage().'. The code: '.$e->getCode());
-        }
+        $commands[] = '-r';
+        $commands[] = $this->frameRate;
+        $commands[] = $this->destinationFolder . 'frame-%d.jpg';
 
         return $commands;
     }
